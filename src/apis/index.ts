1ed456e08971af25e8c58976c5cfa4e82f565e53
Eric Durand-Tremblay <etremblay@kronostechnologies.com>
2020-07-06 10:00:01 -0400

    Initial commit

---
+++ src/apis/index.ts
@@ -0,0 +1,7 @@
+export * from './EventsApi';
+export * from './GatewaysApi';
+export * from './LegacyDocumentApi';
+export * from './LegacyProvisioningApi';
+export * from './MovementApi';
+export * from './TasksApi';
+export * from './UsersApi';
